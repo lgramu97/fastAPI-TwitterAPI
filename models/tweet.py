f3a93afcf835198332498c3417d44b4a693800cf
lgramu97 <gramuglialautaro@gmail.com>
2022-10-30 21:59:54 -0300

    Created tweet model

--- models/tweet.py
+++ models/tweet.py
@@ -1,5 +1,21 @@
 # Pydantinc
-from pydantic import BaseModel
+# Python
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+# Pydantinc
+from pydantic import BaseModel, Field, Required
+# Custom
+from models.users import User
+
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(Required)
+    content: str = Field(
+        Required,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(defualt=None)
+    by: User = Field(Required)
