fc51e951a120f2ac21d260d4db6fe75ff8cad71e
lgramu97 <gramuglialautaro@gmail.com>
2022-10-30 21:55:54 -0300

    Users model created

--- models/users.py
+++ models/users.py
@@ -7,37 +7,39 @@ from datetime import date
 # Pydantinc
 from pydantic import BaseModel, EmailStr, Field, Required
 
+
 class UserBase(BaseModel):
     """
     User basic information.
     """
-    user_id : UUID = Field(Required) 
-    email : EmailStr = Field(Required)
-    
+    user_id: UUID = Field(Required)
+    email: EmailStr = Field(Required)
+
 
 class UserLogin(UserBase):
     """ 
     User login.
     """
-    password : str = Field(
+    password: str = Field(
         Required,
-        min_length=8
-    ) 
+        min_length=8,
+        max_length=64
+    )
+
 
 class User(BaseModel):
     """
     User information.
     """
     # Obligatory attr.
-    first_name : str = Field(
-        Required,
-        min_length=1,
-        max_length=20
-    )    
-    last_name : str = Field(
+    first_name: str = Field(
         Required,
         min_length=1,
         max_length=20
     )
-    birth_date : Optional[date] = Field(default=None)
-    
+    last_name: str = Field(
+        Required,
+        min_length=1,
+        max_length=20
+    )
+    birth_date: Optional[date] = Field(default=None)
