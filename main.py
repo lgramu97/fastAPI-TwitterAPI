86ac0442fbd61294ca866ea892a2895621a94f57
lgramu97 <gramuglialautaro@gmail.com>
2022-10-30 22:10:51 -0300

    Path operations structure - Users

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# Python
+from typing import List
 # FastAPI
-from fastapi import FastAPI
-
+from fastapi import FastAPI, status
 # Custom
 from models.users import User
 from models.tweet import Tweet
@@ -8,8 +9,86 @@ from models.tweet import Tweet
 app = FastAPI()
 
 
+# Path Operations.
+
 @app.get(
     path="/home"
 )
 def home():
     return {"Twitter API": "Working Fine"}
+
+
+# Users
+
+# Signup
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def signup():
+    pass
+
+
+# Login
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def login():
+    pass
+
+
+# All users
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all users",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+
+# One user
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a user",
+    tags=["Users"]
+)
+def show_user():
+    pass
+
+
+# Delete one user
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a user",
+    tags=["Users"]
+)
+def delete_user():
+    pass
+
+
+# Update one user
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a user",
+    tags=["Users"]
+)
+def update_user():
+    pass
+
+# Tweets
